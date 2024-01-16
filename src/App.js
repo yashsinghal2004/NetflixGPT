9855fffcec0581555ac5164b265aa5274d7a49e8
Yash Singhal <118036819+yashsinghal2004@users.noreply.github.com>
2024-01-16 09:07:18 +0000

    Update App.js and Body.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,32 @@ import "./App.css";
 import Body from "./Components/Body";
 import Header from "./Components/Header";
 import appStore from "./Utils/appStore";
+import Login from "./Components/Login";
+import Browse from "./Components/Browse";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-function App() {
+const App = () => {
+  const approuter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/browse",
+      element: <Browse />,
+    },
+  ]);
   return (
     <div className="App">
       <Header />
       <Provider store={appStore}>
         <Body />
+        <div>
+          <RouterProvider router={approuter} />
+        </div>
       </Provider>
     </div>
   );
-}
+};
 
 export default App;
