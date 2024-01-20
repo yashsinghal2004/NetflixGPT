20f557f0ede913aa4b10737cf3703f1ab0060c90
Yash Singhal <118036819+yashsinghal2004@users.noreply.github.com>
2024-01-20 04:54:45 +0000

    bg-trailer

--- src/Components/Videobackground.js
+++ src/Components/Videobackground.js
@@ -1,4 +1,46 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { options } from "../Utils/constants";
+import { addTrailer } from "../Utils/moviesSlice";
+import { useSelector } from "react-redux";
 const Videobackground = () => {
-  return <div>Hello</div>;
+  const trailerfromstore = useSelector((store) => store?.movies?.Trailer);
+
+  const dispatch = useDispatch();
+  const fetchTrailer = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/955916/videos?language=en-US",
+      options,
+    );
+    const json = await data.json();
+    console.log(json);
+    const Trailer = (json?.results).filter((video) => video.type === "Trailer");
+    const MainTrailer = Trailer.length ? Trailer[0] : json.results[0];
+    console.log(MainTrailer);
+    dispatch(addTrailer(MainTrailer));
+  };
+
+  useEffect(() => {
+    fetchTrailer();
+  }, []);
+
+  //console.log(trailerfromstore.key);
+
+  return (
+    <div>
+      <iframe
+        className="w-full h-screen"
+        src={
+          "https://www.youtube.com/embed/" +
+          trailerfromstore?.key +
+          "?si=G00legvSKpkov81o"
+        }
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
 };
 export default Videobackground;
--- src/Utils/moviesSlice.js
+++ src/Utils/moviesSlice.js
@@ -3,12 +3,16 @@ const moviesSlice = createSlice({
   name: "movies",
   initialState: {
     nowPlaying: null,
+    Trailer: null,
   },
   reducers: {
     addNowPlaying: (state, action) => {
       state.nowPlaying = action.payload;
     },
+    addTrailer: (state, action) => {
+      state.Trailer = action.payload;
+    },
   },
 });
-export const { addNowPlaying } = moviesSlice.actions;
+export const { addNowPlaying, addTrailer } = moviesSlice.actions;
 export default moviesSlice.reducer;
