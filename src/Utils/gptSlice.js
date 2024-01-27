6ec1d82e53ecd3e860078980694240aa2c1b0bfe
Yash Singhal <118036819+yashsinghal2004@users.noreply.github.com>
2024-01-27 04:27:13 +0000

    added-gptsearch-results-to store

--- src/Utils/gptSlice.js
+++ src/Utils/gptSlice.js
@@ -3,13 +3,20 @@ const gptSlice = createSlice({
   name: "gpt",
   initialState: {
     Searchgpt: false,
+    GptMovieNames: null,
+    TmdbResults: null,
   },
   reducers: {
     ChangeSearchState: (state, action) => {
       state.Searchgpt = !state.Searchgpt;
     },
+    addGptResult: (state, action) => {
+      const { GptMovieNames, TmdbResults } = action.payload;
+      state.GptMovieNames = GptMovieNames;
+      state.TmdbResults = TmdbResults;
+    },
   },
 });
 
-export const { ChangeSearchState } = gptSlice.actions;
+export const { ChangeSearchState, addGptResult } = gptSlice.actions;
 export default gptSlice.reducer;
