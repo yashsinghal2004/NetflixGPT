616d8df5321eb4a3ce8b2c50a778a7b601255669
Yash Singhal <118036819+yashsinghal2004@users.noreply.github.com>
2024-02-02 12:14:55 +0000

    feat: Remove unused imports and add dispatch hook

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -2,14 +2,8 @@ import React from "react";
 import Login from "./Login";
 import Browse from "./Browse";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { useEffect } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../Utils/firebase";
-import { useDispatch } from "react-redux";
-import { addUser, removeUser } from "../Utils/userSlice";
 
 const Body = () => {
-  const dispatch = useDispatch();
   const approuter = createBrowserRouter([
     {
       path: "/",
